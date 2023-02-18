5e325078398e14d84f63d5278195c2610bd1384b
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-18 21:57:54 +0530

    quizDb created

---
+++ scripts/quiz.js
@@ -0,0 +1,82 @@
+const quesDataBase = [
+    {
+        question: "Q1: What is the full form of HTTP?",
+        a: "Hypertext transfer protocol",
+        b: "Hypertext transfer policy",
+        c: "Hypertext text protocol",
+        d: "Hypertect text policy",
+        ans: "a"
+    },
+    {
+        question: "Q2: Inside which HTML element do we put the JavaScript?",
+        a: "<Scripting>",
+        b: "<Script>",
+        c: "<body>",
+        d: "<head>",
+        ans: "b"
+    },
+    {
+        question: "Q3: Where is the correct place to insert a JavaScript?",
+        a: "Head",
+        b: "body",
+        c: "both head and body",
+        d: "None",
+        ans: "c"
+    },
+    {
+        question: "Q4: What is the correct syntax for referring to an external script called xxx.js ?",
+        a: "<script href = xxx.js>",
+        b: "<script name = xxx.js>",
+        c: "<script link = xxx.js>",
+        d: "<script to = xxx.js>",
+        ans: "a"
+    },
+    {
+        question: "Q5: How do you write Hello World in an alert box ?",
+        a: "alert(Hello world)",
+        b: "conosle.log(alert)",
+        c: "console.log(Hello world)",
+        d: "msg(Hello world)",
+        ans: "a"
+    },
+    {
+        question: "Q6: What is the full form of HTTP ?",
+        a: "Hypertext transfer protocol",
+        b: "Hypertext transfer policy",
+        c: "Hypertext text protocol",
+        d: "Hypertect text policy",
+        ans: "a"
+    },
+    {
+        question: "Q7: How do you create a function in JavaScript ?",
+        a: "function myFunction()",
+        b: "function:myFunction",
+        c: "function:myFunction()",
+        d: "function = myFunction",
+        ans: "a"
+    },
+    {
+        question: "Q8: How do you call a function named myFunction ?",
+        a: "call myFunction",
+        b: "call myFunction()",
+        c: "myFunction()",
+        d: "myFunction",
+        ans: "c"
+    },
+    {
+        question: "Q9: How to write an IF statement in JavaScript ?",
+        a: "if i == 5 then",
+        b: "if(i == 5)",
+        c: "if i = 5 then",
+        d: "if(i = 5)",
+        ans: "b"
+    },
+    {
+        question: "Q10: What is the full form of CSS ?",
+        a: "Cascading style sheets",
+        b: "Cascading style string",
+        c: "Computed style sheet",
+        d: "Computed style string",
+        ans: "a"
+    }
+]
