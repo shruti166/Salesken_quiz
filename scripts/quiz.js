8695e9c4647986085d754902949b1726b244567a
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-19 01:45:42 +0530

    quesdb updated

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,82 +1,82 @@
 const quesDataBase = [
     {
         question: "Q1: What is the full form of HTTP?",
-        a: "Hypertext transfer protocol",
-        b: "Hypertext transfer policy",
-        c: "Hypertext text protocol",
-        d: "Hypertect text policy",
+        a: "a. Hypertext transfer protocol",
+        b: "b. Hypertext transfer policy",
+        c: "c. Hypertext text protocol",
+        d: "d.Hypertect text policy",
         ans: "a"
     },
     {
         question: "Q2: Inside which HTML element do we put the JavaScript?",
-        a: "<Scripting>",
-        b: "<Script>",
-        c: "<body>",
-        d: "<head>",
+        a: "a. <Scripting>",
+        b: "b. <Script>",
+        c: "c. <body>",
+        d: "d. <head>",
         ans: "b"
     },
     {
         question: "Q3: Where is the correct place to insert a JavaScript?",
-        a: "Head",
-        b: "body",
-        c: "both head and body",
-        d: "None",
+        a: "a. Head",
+        b: "b. body",
+        c: "c. both head and body",
+        d: "d. None",
         ans: "c"
     },
     {
         question: "Q4: What is the correct syntax for referring to an external script called xxx.js ?",
-        a: "<script href = xxx.js>",
-        b: "<script name = xxx.js>",
-        c: "<script link = xxx.js>",
-        d: "<script to = xxx.js>",
+        a: "a. <script href = xxx.js>",
+        b: "b. <script name = xxx.js>",
+        c: "c. <script link = xxx.js>",
+        d: "d. <script to = xxx.js>",
         ans: "a"
     },
     {
         question: "Q5: How do you write Hello World in an alert box ?",
-        a: "alert(Hello world)",
-        b: "conosle.log(alert)",
-        c: "console.log(Hello world)",
-        d: "msg(Hello world)",
+        a: "a. alert(Hello world)",
+        b: "b. conosle.log(alert)",
+        c: "c. console.log(Hello world)",
+        d: "d. msg(Hello world)",
         ans: "a"
     },
     {
         question: "Q6: What is the full form of HTTP ?",
-        a: "Hypertext transfer protocol",
-        b: "Hypertext transfer policy",
-        c: "Hypertext text protocol",
-        d: "Hypertect text policy",
+        a: "a. Hypertext transfer protocol",
+        b: "b. Hypertext transfer policy",
+        c: "c. Hypertext text protocol",
+        d: "d. Hypertect text policy",
         ans: "a"
     },
     {
         question: "Q7: How do you create a function in JavaScript ?",
-        a: "function myFunction()",
-        b: "function:myFunction",
-        c: "function:myFunction()",
-        d: "function = myFunction",
+        a: "a. function myFunction()",
+        b: "b. function:myFunction",
+        c: "c. function:myFunction()",
+        d: "d. function = myFunction",
         ans: "a"
     },
     {
         question: "Q8: How do you call a function named myFunction ?",
-        a: "call myFunction",
-        b: "call myFunction()",
-        c: "myFunction()",
-        d: "myFunction",
+        a: "a. call myFunction",
+        b: "b. call myFunction()",
+        c: "c. myFunction()",
+        d: "d. myFunction",
         ans: "c"
     },
     {
         question: "Q9: How to write an IF statement in JavaScript ?",
-        a: "if i == 5 then",
-        b: "if(i == 5)",
-        c: "if i = 5 then",
-        d: "if(i = 5)",
+        a: "a. if i == 5 then",
+        b: "b. if(i == 5)",
+        c: "c. if i = 5 then",
+        d: "d. if(i = 5)",
         ans: "b"
     },
     {
         question: "Q10: What is the full form of CSS ?",
-        a: "Cascading style sheets",
-        b: "Cascading style string",
-        c: "Computed style sheet",
-        d: "Computed style string",
+        a: "a. Cascading style sheets",
+        b: "b. Cascading style string",
+        c: "c. Computed style sheet",
+        d: "d. Computed style string",
         ans: "a"
     }
 ]
